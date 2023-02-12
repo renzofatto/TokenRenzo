ee49ab427df83756f637e1127176014a116d84e6
Renzo Fattorini <80908769+renzofatto@users.noreply.github.com>
2023-02-11 21:18:46 -0300

    Update wallet's name

--- ERC20.sol
+++ ERC20.sol
@@ -5,10 +5,10 @@ import "./SafeMath.sol";
 
 
 // Renzo Fattorini ----> 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-// Hugo Fattorini ----> 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-//Giovanna Paracampo ----> 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c
+// Anne ----> 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// Bob ----> 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c
 
-//Interface de nuestro token IERC20
+//Interface del token IERC20
 interface IERC20{
     //Devuelve la cantidad de tokens en existencia
     function totalSupply() external view returns (uint256);
